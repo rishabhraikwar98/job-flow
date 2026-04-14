1ba31a092487ddad2757d661b1d19fc020de4ed4
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2026-04-14 18:35:13 +0530

    refactor files

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { signIn } from "next-auth/react";
-import { signUpAction } from "./actions";
+import { signUpAction } from "@/actions/auth-actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
